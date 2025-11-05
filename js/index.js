a030a027a1ebf83ebc6155d739591654b7bcc1ee
Ariadna Evans <ariadnaevans@hotmail.com>
2025-11-05 14:51:21 -0300

    Modifique la ruta de las imagenes de los productos

--- js/index.js
+++ js/index.js
@@ -1,88 +1,53 @@
-const arrayProductos = [
-  {
-    nombre: "Balanceado gato adulto (Excellent)",
-    img: "../img/gato adulto.png",
-    descripcion: "La elección del alimento es clave para la salud y energía de tu gato. Este alimento seco Excellent aporta proteínas de alto valor biológico, vitaminas (A, D y E), minerales (calcio, fósforo, potasio, hierro), fibras y ácidos grasos esenciales",
-    precio: 40000,
-  },
-  {
-    nombre: "Balanceado gato senior +7 (Excellent)",
-    img: "../img/gato senior.png",
-    descripcion: "Excellent Gato Adulto Mantenimiento es un alimento balanceado premium formulado con proteínas de alta calidad, vitaminas y minerales que favorecen la vitalidad, el sistema inmunológico y el cuidado del pelo y la piel de tu gato adulto. Su formato seco ayuda a mantener la salud dental y se conserva por más tiempo sin perder propiedades.",
-    precio: 43000,
-  },
-  {
-    nombre: "Balanceado gato cachorro -1 (Excellent)",
-    img: "../img/gato cachorro.png",
-    descripcion: "Alimento Balanceado para Gato Pupy Excellent: formulado con proteínas de alta calidad, vitaminasy minerales esenciales que favorecen el crecimiento sano, fortalecen el sistema inmunológico ymantienen el pelo y la piel de tu gato brillantes y saludables. Su formato seco ayuda a la higiene dental y asegura una nutrición completa todos los días.",
-    precio: 52000,
-  },
-  {
-    nombre: "Balanceado perro raza grande (Excellent)",
-    img: "../img/perro adulto raza grande.png",
-    descripcion: "Excellent Adult de Purina es un alimento seco para perros adultos de raza mediana y grande. Con sabor a pollo y arroz, aporta 24% de proteína para mantener la masa muscular y la energía, además de vitaminas, minerales, calcio y fósforo para el bienestar general. Su fórmula con inulina y fibra de soja favorece la digestión y refuerza el sistema inmunológico.",
-    precio: 60000,
-  },  
-  {
-    nombre: "Balanceado perro raza chica (Excellent)",
-    img: "../img/perro adulto raza pequenia.png",
-    descripcion: "Excellent Perros es un alimento seco balanceado que aporta proteínas de alto valor biológico,vitaminas (A, D, E), minerales y ácidos grasos esenciales. Favorece el sistema inmunológico, la digestión y la salud de músculos, huesos, piel y pelaje, además de ayudar a prevenir sarroy placa dental.",
-    precio: 61000,
-  },
-  {
-    nombre: "Balanceado perro cachorro (Excellent)",
-    img: "../img/perro cachorro.png",
-    descripcion: "Excellent Perros es un alimento seco balanceado que cubre las necesidades nutricionales y energéticas de tu mascota. Aporta proteínas de calidad, vitaminas (A, D, E), minerales y ácidos grasos esenciales que fortalecen el sistema inmunológico, favorecen la digestión y contribuyen a la salud de músculos, huesos, piel, pelaje y dientes.",
-    precio: 59500,
-  },
-];
+import { arrayProductos } from "./productos.js";
+import { agregarAlCarrito } from "./funcionesCarrito.js";
+import { obtenerCarrito } from "./storage.js";
+import { actualizarContador } from "./ui.js";
 
-const seccionProductos = document.getElementById("seccionProductos");
+document.addEventListener("DOMContentLoaded", () => {
+  const seccionProductos = document.getElementById("seccionProductos");
 
-let tuperProductos = document.createElement("div");
-tuperProductos.classList.add("tuperProductos");
+  const carrito = obtenerCarrito();
+  actualizarContador(carrito);
 
-arrayProductos.forEach ((producto) => {
+  arrayProductos.forEach ((producto) => {
 
-  let artProducto = document.createElement("article");
-  artProducto.classList.add("artProducto");
+    const artProducto = document.createElement("article");
+    artProducto.classList.add("artProducto");
 
-  let titulo = document.createElement("h2");
-  titulo.classList.add("titulo");
-  titulo.textContent = producto.nombre;
+    const titulo = document.createElement("h2");
+    titulo.classList.add("titulo");
+    titulo.textContent = producto.nombre;
+    
+    const img = document.createElement("img");
+    img.classList.add("img");
+    img.src = producto.img;
+    img.alt = producto.titulo
+    
+    const descripcion = document.createElement("h3");
+    descripcion.classList.add("descripcion");
+    descripcion.textContent = producto.descripcion;
   
-  let img = document.createElement("img");
-  img.classList.add("img");
-  img.src = producto.img;
-  
-  let descripcion = document.createElement("h3");
-  descripcion.classList.add("descripcion");
-  descripcion.textContent = producto.descripcion;
- 
-  let precio = document.createElement("p");
-  precio.classList.add("precio");
-  precio.textContent = `Precio: $${producto.precio}`;
+    const precio = document.createElement("p");
+    precio.classList.add("precio");
+    precio.textContent = `Precio: $${producto.precio}`;
 
-  let carrito = document.createElement("button");
-  carrito.classList.add("carrito");
+    const boton = document.createElement("button");
+    boton.classList.add("btn");
+    boton.textContent = "Agregar al carrito";
 
-  let imgCarrito = document.createElement("img");
-  imgCarrito.src = "../img/carro-de-la-carretilla.png"; 
-  imgCarrito.alt = "Agregar al carrito";
-  imgCarrito.style.width = "15px";
-  imgCarrito.style.height = "15px";
-  carrito.addEventListener("click", () => {
-    alert(`✅ ${producto.nombre} agregado a tu carrito`);
-  });
-  carrito.appendChild(imgCarrito);
+    boton.addEventListener("click", () => {
+      agregarAlCarrito(producto);
+    });
 
-  artProducto.appendChild(titulo);
-  artProducto.appendChild(img);
-  artProducto.appendChild(descripcion);
-  artProducto.appendChild(precio);
-  artProducto.appendChild(carrito);
-  
-  tuperProductos.appendChild(artProducto);
+    artProducto.appendChild(titulo);
+    artProducto.appendChild(img);
+    artProducto.appendChild(descripcion);
+    artProducto.appendChild(precio);
+    artProducto.appendChild(boton);
+
+    seccionProductos.appendChild(artProducto)
+
+  })
+
 })
-
-seccionProductos.appendChild(tuperProductos);
+  
